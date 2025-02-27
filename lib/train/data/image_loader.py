62d32532a19f14bfaed672a52b6d6b226baf55e9
wrs <416408606.com>
2025-02-27 16:13:36 +0800

    使用pil库代替原有的jpeg4py库读取图片

--- lib/train/data/image_loader.py
+++ lib/train/data/image_loader.py
@@ -2,6 +2,8 @@ import jpeg4py
 import cv2 as cv
 from PIL import Image
 import numpy as np
+from PIL import Image
+
 
 davis_palette = np.repeat(np.expand_dims(np.arange(0,256), 1), 3, 1).astype(np.uint8)
 davis_palette[:22, :] = [[0, 0, 0], [128, 0, 0], [0, 128, 0], [128, 128, 0],
@@ -34,7 +36,8 @@ default_image_loader.use_jpeg4py = None
 def jpeg4py_loader(path):
     """ Image reading using jpeg4py https://github.com/ajkxyz/jpeg4py"""
     try:
-        return jpeg4py.JPEG(path).decode()
+        # return jpeg4py.JPEG(path).decode()
+        return np.array(Image.open(path).convert("RGB"))
     except Exception as e:
         print('ERROR: Could not read image "{}"'.format(path))
         print(e)
